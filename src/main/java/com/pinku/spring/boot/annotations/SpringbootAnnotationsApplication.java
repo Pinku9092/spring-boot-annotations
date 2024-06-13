32f42c668952e28a21c03fed265642c73109db2e
Pinku <kpinku32@gmail.com>
2024-06-13 18:11:50 +0530

    first commit

---
+++ src/main/java/com/pinku/spring/boot/annotations/SpringbootAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.pinku.spring.boot.annotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAnnotationsApplication.class, args);
+	}
+
+}
